c00940641b0ae4920fd279ef8becdc8094f31efc
AidanMcB <aidankmcbride@gmail.com>
2020-08-14 23:50:39 -0400

    access to youtube api search and find by id

--- src/App.js
+++ src/App.js
@@ -8,42 +8,35 @@ import Homepage from './components/Homepage'
 import axios from 'axios';
 
 function App() {
+  //q= is equivelant to the input search term, API key must be generated with google account
+  // Search Result Paramaters with my API key
+  // Google Developer Console => API dashboard
+  // Credentials => +Create Credentials (restrict key if you want)
+  //https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=lego&key=AIzaSyBkRxNBktlEpXjJcwPQRHwvRgYYXP3MOiQ
 
+
+  //OR search by video ID, available from searchResult response 
+  //https://www.googleapis.com/youtube/v3/videos?part=snippet&id=Tqth0fKo0_g&maxResults=5&key=AIzaSyBkRxNBktlEpXjJcwPQRHwvRgYYXP3MOiQ
   const key = `AIzaSyBkRxNBktlEpXjJcwPQRHwvRgYYXP3MOiQ`
 
-  // const getYTdata = () => {
-  //   fetch(`https://www.googleapis.com/youtube/v3/snippet/5/${key}`)
-  //     .then( res => res.json())
-  //     .then( data => console.log(data))
-  // }
+  const getYTdata = (e, search) => {
+    e.preventDefault()
+    // debugger
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${search}&key=${key}`)
+      .then( res => res.json())
+      .then( data => console.log(data))
+  }
   const [videos, setVideos] = useState([])
   const [searchTerm, setTerm] = useState('')
     
-    const handleSubmit = async (e,search) => {
-      debugger
-      e.preventDefault()
-      const response = await youtube.get('/search', {
-        params: {
-          q: search
-        }
-      })
-        setVideos({
-        videos: response.data.items
-      })
-      console.log("this is resp", response);
-    } 
-  
-
-
-
   return (
     <div className="App">
         <Homepage />
-        <form onSubmit={(e, search) => handleSubmit(e, search)}>
+        <form onSubmit={(e) => getYTdata(e, searchTerm)}>
           Search
-          <input name="search" ></input>
+          <input name="search" 
+          onChange={(e) => setTerm( e.target.value )}></input>
         </form>
-        <button onClick={() => console.log("test")}>Data Button</button>
     </div>
   );
 }
