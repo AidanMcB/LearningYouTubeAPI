a925083d9984a4ca2a9ca02942ad49aaf05afa18
AidanMcB <aidankmcbride@gmail.com>
2020-08-15 16:21:33 -0400

    working but youtube request limit

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 // Components
-import youtube from './components/youtube'
 import Homepage from './components/Homepage'
-import axios from 'axios';
 
 function App() {
 
@@ -28,17 +26,21 @@ function App() {
   const getYTdata = (e, search) => {
     e.preventDefault()
     // debugger
+    //paramaters 
+    //title
+    //jpeg 
+    //videoId
     fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${search}&key=${key}`)
       .then( res => res.json())
       .then( video => {
-         setVideo(video)
+        setVideo(video)
       })
   }
   const [video, setVideo] = useState({})
   const [searchTerm, setTerm] = useState('')
   // const vidSrc = `https://www.youtube.com/embeded/${video.id.videoId}`
   return (
-    console.log(video.items),
+    console.log("the video is:", video),
     <div className="App">
         <Homepage />
         <form onSubmit={(e) => getYTdata(e, searchTerm)}>
@@ -46,7 +48,9 @@ function App() {
           <input name="search" 
           onChange={(e) => setTerm( e.target.value )}></input>
         </form>
-        {video.items == undefined ? null : <iframe src={`https://www.youtube.com/embed/${video.items[0].id.videoId}`} allowFullScreen title="Video player" /> }
+        {/* logic to check if video.items(the array of videos) is undefined */}
+        {/* if it is undefined, render nothing, if there is a video in state, render the iframe */}
+        {video.items == undefined ? null : <iframe src={`https://www.youtube.com/embed/${video.items[0].id.videoId}`} allowFullScreen width="300px" height="500px" title="Video player" /> }
     </div>
   );
 }
